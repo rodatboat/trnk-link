4cf13c1657e6b7db5176627b5e0dcbebcb6c75b4
Ronaldo Suarez <ronaldo3055@gmail.com>
2023-04-17 11:18:31 -0400

    fix cors

--- server/server.js
+++ server/server.js
@@ -27,6 +27,22 @@ mongoose.connect(mongoUrl)
     .catch((e) => console.log(e))
 mongoose.set('strictQuery', true);
 
+app.use(function (req, res, next) {
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 const usersRouter = require('./routes/users');
 const componentsRouter = require('./routes/components');
 
